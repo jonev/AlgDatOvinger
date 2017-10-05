ba1f6de1619e80afef9375a8ac1b2c2cde4f1bd4
jonev <jonev@stud.ntnu.no>
2017-10-05 23:40:20 +0200

    Oppg1 nesten klar. Mangler å klare lese inn filene med de rare strukturene.

--- src/TestCode/oving7Uvektedegrafer/GrafTest.java
+++ src/TestCode/oving7Uvektedegrafer/GrafTest.java
@@ -7,10 +7,10 @@ import org.junit.Test;
 import static org.junit.Assert.*;
 
 public class GrafTest {
-    Graf instance;
+    Graph instance;
     @Before
     public void setUp() throws Exception {
-        instance = new Graf();
+        instance = new Graph();
     }
 
     @After
@@ -19,16 +19,73 @@ public class GrafTest {
     }
 
     @Test
-    public void readGraphFile() throws Exception {
-        instance.readGraphFile("src/oving7Uvektedegrafer/L7g1.txt");
-        int[][] print = instance.getEdgeTbl();
-        for (int i = 0; i < instance.getEdgeTbl().length; i++) {
-            System.out.println(print[i][0] + " " + print[i][1]);
+    public void breddeforstsok() throws Exception {
+        instance.readGraphFile("src/oving7Uvektedegrafer/testgraph.txt");
+        Node[] nodetbl = o7.breddeforstsok(instance, 5);
+        for (int i = 0; i < nodetbl.length; i++) {
+            System.out.println("Node " + nodetbl[i].getNr() + " forgjenger " + nodetbl[i].getPredecessor() + " dist " + nodetbl[i].getDist());
         }
-        int[] node = instance.getIndexInEdgeTbl();
-        for (int i = 0; i < node.length; i++) {
-            System.out.println(node[i]);
-        }
+
+        assertEquals(2, nodetbl[0].getPredecessor());
+        assertEquals(2, nodetbl[0].getDist());
     }
 
+    @Test
+    public void readGraphFile() throws Exception {
+        instance.readGraphFile("src/oving7Uvektedegrafer/testgraph.txt");
+
+
+        System.out.println(instance.toString());
+
+        assertEquals(17, instance.getEdgeTbl().length);
+        assertEquals(2, instance.getEdgeTbl()[0]);
+        assertEquals(4, instance.getEdgeTbl()[16]);
+        assertEquals(8, instance.getIndexInEdgeTbl().length);
+        assertEquals(0, instance.getIndexInEdgeTbl()[0].getIndex());
+        assertEquals(2, instance.getIndexInEdgeTbl()[1].getIndex());
+        assertEquals(2, instance.getIndexInEdgeTbl()[2].getIndex());
+        assertEquals(5, instance.getIndexInEdgeTbl()[3].getIndex());
+    }
+
+
+    @Test
+    public void L7g1() throws Exception {
+        Graph L7g1 = new Graph();
+        L7g1.readGraphFile("src/oving7Uvektedegrafer/L7g1.txt");
+        Node[] nodetbl = o7.breddeforstsok(L7g1, 5);
+        System.out.println("Resultat av L7g1.txt ------------");
+        for (int i = 0; i < nodetbl.length-1; i++) {
+            System.out.println("Node " + nodetbl[i].getNr() + " forgjenger " + nodetbl[i].getPredecessor() + " dist " + nodetbl[i].getDist());
+        }
+        System.out.println("------------------------------------");
+
+    }
+
+    @Test
+    public void L7g2() throws Exception {
+        Graph L7g2 = new Graph();
+        L7g2.readGraphFile("src/oving7Uvektedegrafer/L7g2.txt");
+        Node[] nodetbl = o7.breddeforstsok(L7g2, 5);
+        System.out.println("Resultat av L7g1.txt ------------");
+        System.out.println("Noder: " + L7g2.getNodecount() + " kanter: " + L7g2.getEdgecount());
+        for (int i = 0; i < nodetbl.length-1; i++) {
+            System.out.println("Node " + nodetbl[i].getNr() + " forgjenger " + nodetbl[i].getPredecessor() + " dist " + nodetbl[i].getDist());
+        }
+        System.out.println("------------------------------------");
+
+    }
+
+    @Test
+    public void L7g3() throws Exception {
+        Graph L7g3 = new Graph();
+        L7g3.readGraphFile("src/oving7Uvektedegrafer/L7g3.txt");
+        Node[] nodetbl = o7.breddeforstsok(L7g3, 5);
+        System.out.println("Resultat av L7g1.txt ------------");
+        System.out.println("Noder: " + L7g3.getNodecount() + " kanter: " + L7g3.getEdgecount());
+        for (int i = 0; i < nodetbl.length-1; i++) {
+            System.out.println("Node " + nodetbl[i].getNr() + " forgjenger " + nodetbl[i].getPredecessor() + " dist " + nodetbl[i].getDist());
+        }
+        System.out.println("------------------------------------");
+
+    }
 }
--- src/oving7Uvektedegrafer/o7.java
+++ src/oving7Uvektedegrafer/o7.java
@@ -2,17 +2,51 @@ package oving7Uvektedegrafer;
 
 import java.io.BufferedReader;
 import java.io.FileReader;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.concurrent.ExecutionException;
 
 public class o7 {
+    public static void main(String[] args) throws Exception{
+
+    }
+
+
+    public static Node[] breddeforstsok(Graph graph, int startnode) throws Exception{
+        Node[] nodetbl = graph.getIndexInEdgeTbl();
+        int[] edges = graph.getEdgeTbl();
+        Node s = nodetbl[startnode];
+        s.setDist(0);
+        s.setPredecessor(0);
+        LinkedList<Node> que = new LinkedList<Node>();
+        que.push(s);
+        int[][] result = new int[nodetbl.length][3]; // Node/Forgjenger/Dist
+
+        while (!que.isEmpty()){
+            Node current = que.pollLast();
+            for (int nabonr = current.getIndex(); nabonr < nodetbl[current.getNr()+1].getIndex(); nabonr++) {
+                if(nodetbl[edges[nabonr]].getDist() == Integer.MAX_VALUE){
+                    nodetbl[edges[nabonr]].setDist(current.getDist() + 1);
+                    nodetbl[edges[nabonr]].setPredecessor(current.getNr());
+                    que.push(nodetbl[edges[nabonr]]);
+                }
+            }
+        }
+        return nodetbl;
+    }
 }
 
 
-class Graf{
-    private int[] indexInEdgeTbl;
-    private int[][] edgeTbl;
+class Graph{
+    private Node[] indexInEdgeTbl;
+    private int[] edgeTbl;
+    private int nodecount;
+    private int edgecount;
 
-    public Graf(){
+    public Graph(){
     }
+
+
 
     public void readGraphFile(String filename){
         BufferedReader rd = null;
@@ -21,11 +55,11 @@ class Graf{
             fr = new FileReader(filename);
             rd = new BufferedReader(fr);
 
-            String line = rd.readLine();
-            String[] heading = line.split(" ");
-            int nodecount = Integer.parseInt(heading[0]);
-            int edgecount = Integer.parseInt(heading[1]);
-            indexInEdgeTbl = new int[nodecount];
+            String line = rd.readLine().trim();
+            String[] heading = line.split(",");
+            nodecount = Integer.parseInt(heading[0].trim());
+            edgecount = Integer.parseInt(heading[1].trim());
+            indexInEdgeTbl = new Node[nodecount+1];
             int[][] unsortedEdgTbl = new int[edgecount][2];
 
             int node = -1;
@@ -33,41 +67,38 @@ class Graf{
             int to = -1;
             int index = 0;
 
+            // read from file
             while ((line = rd.readLine()) != null){
-                String splitt[] = line.split(" ");
-                from = Integer.parseInt(splitt[0]);
-                to = Integer.parseInt(splitt[1]);
+                String splitt[] = line.trim().split(",");
+                from = Integer.parseInt(splitt[0].trim());
+                to = Integer.parseInt(splitt[1].trim());
 
                 unsortedEdgTbl[index][0] = from;
                 unsortedEdgTbl[index++][1] = to;
             }
 
-            // sort
-            edgeTbl = new int[edgecount][2];
-            int u = 0;
-            for (int i = 0; i < nodecount; i++) {
-                for (int j = 0; j < edgecount; j++) {
-                    if(unsortedEdgTbl[j][0] == i){
+            // sort edges
+            boolean hasEdge = false;
+            edgeTbl = new int[edgecount];
+            int edgetblindex = 0;
+            for (int nodenrindex = 0; nodenrindex < nodecount; nodenrindex++) { // for each node
+                indexInEdgeTbl[nodenrindex] = new Node(nodenrindex);
+                indexInEdgeTbl[nodenrindex].setIndex(edgetblindex);
+                hasEdge = false;
+                for (int edgeindex = 0; edgeindex < edgecount; edgeindex++) { // collection all edges for that node
+                    if(unsortedEdgTbl[edgeindex][0] == nodenrindex){
                         try {
-                            edgeTbl[u][0] = unsortedEdgTbl[j][0];
-                            edgeTbl[u][1] = unsortedEdgTbl[j][1];
-                            u++;
+                            edgeTbl[edgetblindex] = unsortedEdgTbl[edgeindex][1];
+                            edgetblindex++;
                         }catch (Exception e){
-
+                            e.printStackTrace();
                         }
                     }
                 }
             }
+            // adding dummynode
+            indexInEdgeTbl[nodecount] = new Node(nodecount);
 
-            index = 0;
-            int nodenr = 0;
-            indexInEdgeTbl[index] = nodenr;
-            for (int i = 0; i < edgecount; i++) {
-                if(edgeTbl[i][0] != nodenr){
-                    nodenr = edgeTbl[i][0];
-                    indexInEdgeTbl[++index] = i;
-                }
-            }
 
         }catch(Exception e){
             e.printStackTrace();
@@ -81,11 +112,76 @@ class Graf{
         }
     }
 
-    public int[] getIndexInEdgeTbl() {
+    public Node[] getIndexInEdgeTbl() {
         return indexInEdgeTbl;
     }
 
-    public int[][] getEdgeTbl() {
+    public int[] getEdgeTbl() {
         return edgeTbl;
     }
+
+    public String toString(){
+        String s = "Nodes: " + nodecount + " edges " + edgecount;
+        for (int i = 0; i < indexInEdgeTbl.length-1; i++) {
+            for (int j = indexInEdgeTbl[i].getIndex(); j < indexInEdgeTbl[i+1].getIndex(); j++) {
+                s += "\nNode " + i + " kant til " + edgeTbl[j];
+            }
+        }
+        return s;
+    }
+
+
+    public int getNodecount() {
+        return nodecount;
+    }
+
+    public int getEdgecount() {
+        return edgecount;
+    }
+}
+
+class Node{
+    private int dist = Integer.MAX_VALUE;
+    private int nr;
+    private int index = -1;
+    int predecessor = -1;
+
+    public Node(int nr){
+        this.nr = nr;
+    }
+
+    public int getPredecessor() {
+        return predecessor;
+    }
+
+    public void setPredecessor(int predecessor) {
+        this.predecessor = predecessor;
+    }
+
+    public void setDist(int dist) {
+        this.dist = dist;
+    }
+
+    public void setNr(int nr) {
+        this.nr = nr;
+    }
+
+    public void setIndex(int index) {
+        this.index = index;
+    }
+
+    public int getDist() {
+        return dist;
+    }
+
+    public int getIndex() {
+        return index;
+    }
+
+    public int getNr() {
+        return nr;
+    }
+
+
+
 }
